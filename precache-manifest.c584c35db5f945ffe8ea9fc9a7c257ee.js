ac826d0d2ae22dcaf26712a4abb89bd3e47edab5
Max Pinheiro <maxpinheiro181@gmail.com>
2022-02-22 03:14:55 -0500

    Updates

---
+++ precache-manifest.c584c35db5f945ffe8ea9fc9a7c257ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8701d6d7fd0a80dbd3448192cfff816f",
+    "url": "/spotify-recap/index.html"
+  },
+  {
+    "revision": "be87344fe85735771262",
+    "url": "/spotify-recap/static/css/main.546f3d28.chunk.css"
+  },
+  {
+    "revision": "7cfd617d035f72e2ff03",
+    "url": "/spotify-recap/static/js/2.894a6f19.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/spotify-recap/static/js/2.894a6f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be87344fe85735771262",
+    "url": "/spotify-recap/static/js/main.0dad6e35.chunk.js"
+  },
+  {
+    "revision": "f3ad329732439aa2140c",
+    "url": "/spotify-recap/static/js/runtime-main.8b14ccd5.js"
+  }
+]);
